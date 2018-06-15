87f97c7608737d6a39d7bbabbcc7fc438b6c5efe
William Decker <willdecker@gmail.com>
2018-06-15 04:57:55 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  const uppercase = string.toUpperCase();
+  console.log(uppercase);
+}
+
+function logWhisper(string) {
+  const lowercase = string.toLowerCase();
+  console.log(lowercase);
+}
+
+function sayHiToGrandma(string) {
+  
+}
